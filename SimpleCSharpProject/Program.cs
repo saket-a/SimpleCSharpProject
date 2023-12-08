f5e6804db1e8585ab8a105f68d77b8fe02bf9ed1
Saket Agrawal <127473091+saket-a@users.noreply.github.com>
2023-12-08 14:18:40 +0530

    Add files via upload

---
+++ SimpleCSharpProject/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleCSharpProject
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Simple Addition: " + Add(2, 3));
+        }
+
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
